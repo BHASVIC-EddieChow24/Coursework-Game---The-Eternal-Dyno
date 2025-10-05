97b2435be2521da96804599b8b27997243d62c20
BHASVIC-EddieChow24 <eddie.chow24@students.bhasvic.ac.uk>
2025-10-05 23:48:21 +0100

    Window now opens

--- eternaldyno/src/MainMenu.java
+++ eternaldyno/src/MainMenu.java
@@ -1,8 +1,19 @@
 import javax.swing.*;
 
-public class MainMenu extends JFrame{
-    public static void main(String[] args) {
+public class MainMenu extends JFrame {
+    private JPanel Background;
+    private JButton startbutton;
 
-        
+
+    public MainMenu() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(Background);
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new MainMenu();
+        frame.setVisible(true); //opens the window
     }
 }
