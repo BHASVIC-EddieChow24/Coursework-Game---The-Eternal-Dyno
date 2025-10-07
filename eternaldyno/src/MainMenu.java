2c59f72ce59019611443fdd637e698c8d640c875
BHASVIC-EddieChow24 <eddie.chow24@students.bhasvic.ac.uk>
2025-10-07 23:51:16 +0100

    Background works

--- eternaldyno/src/MainMenu.java
+++ eternaldyno/src/MainMenu.java
@@ -1,15 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu extends JFrame {
-    private JPanel Background;
-    private JButton startbutton;
-
+    private JLabel Background;
+    private JPanel background;
+    private JButton lbbutton;
+    private JButton settingsbutton;
+    private JButton customisebutton;
+    private JButton usernamebutton;
 
     public MainMenu() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(Background);
-        this.setSize(800, 600);
+        this.setSize(1200, 800);
         this.setLocationRelativeTo(null);
+
+        ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/background img.png"));
+        Background = new JLabel(backgroundIcon);
+        Background.setBounds(0, 0, 1200, 800);
+
+        add(Background);
     }
 
     public static void main(String[] args) {
