f64a1af969b848a49123da2643578b04283385eb
BHASVIC-EddieChow24 <eddie.chow24@students.bhasvic.ac.uk>
2025-10-08 11:41:48 +0100

    added a transparent button

--- eternaldyno/src/MainMenu.java
+++ eternaldyno/src/MainMenu.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 
 public class MainMenu extends JFrame {
     private JLabel Background;
-    private JPanel background;
+    private JButton playbutton;
     private JButton lbbutton;
     private JButton settingsbutton;
     private JButton customisebutton;
@@ -16,15 +16,28 @@ public class MainMenu extends JFrame {
         this.setSize(1200, 800);
         this.setLocationRelativeTo(null);
 
-        ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/background img.png"));
+        ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/background img school.jpg"));
         Background = new JLabel(backgroundIcon);
         Background.setBounds(0, 0, 1200, 800);
 
+        ImageIcon playbuttonicon = new ImageIcon(getClass().getResource("/PlayButton.png"));
+        Image resizebuttonicon = playbuttonicon.getImage().getScaledInstance(200,150, Image.SCALE_SMOOTH);
+        ImageIcon playbuttonicon1 = new ImageIcon(resizebuttonicon);
+        playbutton = new JButton(playbuttonicon1);
+        playbutton.setBounds(500,500,200,150);
+
+        playbutton.setContentAreaFilled(false);
+        playbutton.setBorderPainted(false);
+
+        add(playbutton);
         add(Background);
+
+
     }
 
     public static void main(String[] args) {
         JFrame frame = new MainMenu();
         frame.setVisible(true); //opens the window
     }
+
 }
