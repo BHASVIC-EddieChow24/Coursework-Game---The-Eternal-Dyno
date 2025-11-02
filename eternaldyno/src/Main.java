9f918fda2cbed6afe31aa53dd387327a49e17528
BHASVIC-EddieChow24 <eddie.chow24@students.bhasvic.ac.uk>
2025-11-02 23:20:10 +0000

    started stage 2

---
+++ eternaldyno/src/Main.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new MainMenu();
+        frame.setVisible(true); //opens the window
+    }
+
+}
+
--- eternaldyno/src/MainMenu.java
+++ eternaldyno/src/MainMenu.java
@@ -14,6 +14,7 @@ public class MainMenu extends JFrame {
 
     public MainMenu() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
         this.setSize(1550, 1175);
         this.setLocationRelativeTo(null);
 
@@ -22,11 +23,11 @@ public class MainMenu extends JFrame {
         Background.setBounds(0, 0, 1600, 1200);
 
         ImageIcon playicon = new ImageIcon(getClass().getResource("/Playbutton1.png"));
-        Image resizedplayicon = playicon.getImage().getScaledInstance(200,150, Image.SCALE_SMOOTH);
+        Image resizedplayicon = playicon.getImage().getScaledInstance(200, 150, Image.SCALE_SMOOTH);
         ImageIcon finalplayicon = new ImageIcon(resizedplayicon);
         playbutton = new JButton(finalplayicon);
 
-        playbutton.setBounds(880,700,200,150);
+        playbutton.setBounds(880, 700, 200, 150);
         playbutton.setContentAreaFilled(false);
         playbutton.setBorderPainted(false);
 
@@ -38,11 +39,11 @@ public class MainMenu extends JFrame {
         });
 
         ImageIcon settingsicon = new ImageIcon(getClass().getResource("/settingsbutton.jpg"));
-        Image resizedsettingsicon = settingsicon.getImage().getScaledInstance(175,150, Image.SCALE_SMOOTH);
+        Image resizedsettingsicon = settingsicon.getImage().getScaledInstance(175, 150, Image.SCALE_SMOOTH);
         ImageIcon finalsettingsicon = new ImageIcon(resizedsettingsicon);
         settingsbutton = new JButton(finalsettingsicon);
 
-        settingsbutton.setBounds(1220,75,150,150);
+        settingsbutton.setBounds(1300, 75, 150, 150);
         settingsbutton.setContentAreaFilled(false);
         settingsbutton.setBorderPainted(false);
 
@@ -54,11 +55,11 @@ public class MainMenu extends JFrame {
         });
 
         ImageIcon lbicon = new ImageIcon(getClass().getResource("/leaderboardbutton.png"));
-        Image resizedlbicon = lbicon.getImage().getScaledInstance(150,120, Image.SCALE_SMOOTH);
+        Image resizedlbicon = lbicon.getImage().getScaledInstance(150, 120, Image.SCALE_SMOOTH);
         ImageIcon finallbicon = new ImageIcon(resizedlbicon);
         lbbutton = new JButton(finallbicon);
 
-        lbbutton.setBounds(1200,700,200,150);
+        lbbutton.setBounds(1200, 700, 200, 150);
         lbbutton.setContentAreaFilled(false);
         lbbutton.setBorderPainted(false);
 
@@ -70,11 +71,11 @@ public class MainMenu extends JFrame {
         });
 
         ImageIcon customiseicon = new ImageIcon(getClass().getResource("/customisebutton.png"));
-        Image resizedcustomiseicon = customiseicon.getImage().getScaledInstance(150,120, Image.SCALE_SMOOTH);
+        Image resizedcustomiseicon = customiseicon.getImage().getScaledInstance(150, 120, Image.SCALE_SMOOTH);
         ImageIcon finalcustomiseicon = new ImageIcon(resizedcustomiseicon);
         customisebutton = new JButton(finalcustomiseicon);
 
-        customisebutton.setBounds(550,700,200,150);
+        customisebutton.setBounds(550, 700, 200, 150);
         customisebutton.setContentAreaFilled(false);
         customisebutton.setBorderPainted(false);
 
@@ -86,11 +87,11 @@ public class MainMenu extends JFrame {
         });
 
         ImageIcon usernameicon = new ImageIcon(getClass().getResource("/usernamebutton.png"));
-        Image resizedusernameicon = usernameicon.getImage().getScaledInstance(150,120, Image.SCALE_SMOOTH);
+        Image resizedusernameicon = usernameicon.getImage().getScaledInstance(150, 120, Image.SCALE_SMOOTH);
         ImageIcon finalusernameicon = new ImageIcon(resizedusernameicon);
         usernamebutton = new JButton(finalusernameicon);
 
-        usernamebutton.setBounds(1225,400,150,120);
+        usernamebutton.setBounds(1225, 400, 150, 120);
         usernamebutton.setContentAreaFilled(false);
         usernamebutton.setBorderPainted(false);
 
@@ -102,8 +103,8 @@ public class MainMenu extends JFrame {
         });
 
         Title = new JLabel("THE ETERNAL DYNO");
-        Title.setBounds(445,70,1000,200);
-        Title.setBackground(new Color(0,0,0, 0));
+        Title.setBounds(445, 70, 1000, 200);
+        Title.setBackground(new Color(0, 0, 0, 0));
         Title.setForeground(Color.BLACK);
         Title.setBorder(null);
 
@@ -117,10 +118,4 @@ public class MainMenu extends JFrame {
         add(playbutton);
         add(Background);
     }
-
-    public static void main(String[] args) {
-        JFrame frame = new MainMenu();
-        frame.setVisible(true); //opens the window
-    }
-
 }
